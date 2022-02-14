c95d46911a18efa9a3770979150a70ba30ee3aed
Aymane Silini <silinia@iut1i-702-a04.u-ga.fr>
2022-02-14 10:40:30 +0100

    lgines

--- ProjetTag/src/app/lignes/lignes.page.ts
+++ ProjetTag/src/app/lignes/lignes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-lignes',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lignes.page.scss'],
 })
 export class LignesPage implements OnInit {
+  color:string;
+  dataLigne = [];
 
-  constructor() { }
+  constructor(private api:ApiService) { 
+  }
 
   ngOnInit() {
+    this.api.getData().subscribe(data=>{
+      this.dataLigne = data;
+    })
+    
   }
+
+
 
 }
