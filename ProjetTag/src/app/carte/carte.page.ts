d19e7b49374a7cf80992c71a5a828c588447d52e
Aymane Silini <silinia@iut1i-702-a04.u-ga.fr>
2022-02-15 17:27:31 +0100

    position

--- ProjetTag/src/app/carte/carte.page.ts
+++ ProjetTag/src/app/carte/carte.page.ts
@@ -27,10 +27,14 @@ export class CartePage implements OnInit {
   infoTramC = [];
   infoTramD = [];
   infoTramE = [];
+  markers=[];
+  groupMarkers:any;
+
   latitudePosition; //latitude
   longitudePosition; //longitude
   long:string;
   lat:string;
+
   constructor(private api:ApiService, private geolocation: Geolocation) { 
     this.darkmap = 'https://data.mobilites-m.fr/carte/{z}/{x}/{y}.png';
     this.clearmap = 'https://data.mobilites-m.fr/carte-dark/{z}/{x}/{y}.png'
@@ -79,39 +83,56 @@ export class CartePage implements OnInit {
      });
   }
 
-  research(){
-    console.log(this.search);
-  }
-
-  ionViewDidEnter() { this.leafletMap(); }
-
-  leafletMap() {
-    this.map = Leaflet.map('mapId').setView([ 45.195721,5.706785], 13);
-    Leaflet.tileLayer(this.clearmap, {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
-
+  createMarkers(){
     var iconbus = Leaflet.icon({
       iconUrl: '/assets/bus.png',
       iconSize: [40, 40]
       }); 
-    var  iconUser = Leaflet.icon({
+
+    if(this.map.getZoom()>15 && this.map.hasLayer(this.groupMarkers)==false){
+    for (let k=0;k<this.dataArret.length;k++){
+      if (this.dataArret[k]["properties"]["COMMUNE"] == "GRENOBLE" || this.dataArret[k]["properties"]["COMMUNE"] == "ÉCHIROLLES" || 
+      this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-MARTIN-D'HÈRES" || this.dataArret[k]["properties"]["COMMUNE"] == "SEYSSINS"
+      || this.dataArret[k]["properties"]["COMMUNE"] == "SEYSSINET-PARISET" || this.dataArret[k]["properties"]["COMMUNE"] == "FONTAINE" 
+      || this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-MARTIN-LE-VINOUX"|| this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-ÉGRÈVE" 
+      || this.dataArret[k]["properties"]["COMMUNE"] == "FONTANIL-CORNILLON"|| this.dataArret[k]["properties"]["COMMUNE"] == "LA TRONCHE"
+      || this.dataArret[k]["properties"]["COMMUNE"] == "GIÈRES" || this.dataArret[k]["properties"]["COMMUNE"] == "LE PONT-DE-CLAIX"){
+        this.markers.push(Leaflet.marker([this.dataArret[k]["geometry"]["coordinates"][1], this.dataArret[k]["geometry"]["coordinates"][0]],{icon:iconbus}).addTo(this.map).bindPopup(this.dataArret[k]["properties"]["LIBELLE"]));
+      }
+    }
+    this.groupMarkers = Leaflet.layerGroup(this.markers).addTo(this.map);
+  }
+  if(this.map.getZoom()<=15){
+    if(this.map.hasLayer(this.groupMarkers)){
+      this.map.removeLayer(this.groupMarkers);
+    }
+  }
+  
+}
+
+
+  research(){
+    console.log(this.search);
+  }
+
+  ionViewDidEnter() { 
+    this.leafletMap(); 
+    this.map.on("zoomend",this.createMarkers.bind(this));
+  }
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId').setView([ this.latitudePosition,this.longitudePosition],13);
+    Leaflet.tileLayer(this.clearmap, {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+   
+    var iconUser = Leaflet.icon({
         iconUrl: '/assets/user.gif',
         iconSize: [40, 40]
-        });
+        });   
 
-        for (let k=0;k<this.dataArret.length;k++){
-           //console.log(this.dataArret[k]["properties"]["LIBELLE"]);
-           if (this.dataArret[k]["properties"]["COMMUNE"] == "GRENOBLE" || this.dataArret[k]["properties"]["COMMUNE"] == "ÉCHIROLLES" || this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-MARTIN-D'HÈRES" || this.dataArret[k]["properties"]["COMMUNE"] == "SEYSSINS"
-           || this.dataArret[k]["properties"]["COMMUNE"] == "SEYSSINET-PARISET" || this.dataArret[k]["properties"]["COMMUNE"] == "FONTAINE" || this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-MARTIN-LE-VINOUX"
-           || this.dataArret[k]["properties"]["COMMUNE"] == "SAINT-ÉGRÈVE" || this.dataArret[k]["properties"]["COMMUNE"] == "FONTANIL-CORNILLON"|| this.dataArret[k]["properties"]["COMMUNE"] == "LA TRONCHE"
-           || this.dataArret[k]["properties"]["COMMUNE"] == "GIÈRES" || this.dataArret[k]["properties"]["COMMUNE"] == "LE PONT-DE-CLAIX"){
-           Leaflet.marker([this.dataArret[k]["geometry"]["coordinates"][1], this.dataArret[k]["geometry"]["coordinates"][0]],{icon:iconbus}).addTo(this.map).bindPopup(this.dataArret[k]["properties"]["LIBELLE"]);
-           }
-      }
-
-      Leaflet.marker([this.latitudePosition, this.longitudePosition],{icon:iconUser}).addTo(this.map);
-
+      Leaflet.marker([this.latitudePosition, this.longitudePosition],{icon:iconUser}).addTo(this.map).bindPopup("Votre position");
 
       //pour inverser la latitude et la longitude de A
       for (let k=0;k<this.trajetTramA.length;k++){
@@ -196,14 +217,11 @@ export class CartePage implements OnInit {
       }).addTo(this.map);
     
   }
-
-
-
-
 }
-    
-
-  
-
-
 }
+
+
+
+
+
+//afficher uniquement les points proches avec : "https://data.mobilites-m.fr/api/bbox/json?xmin=5.7029&xmax=5.71146&ymin=45.10554&ymax=45.223&types=arret"
