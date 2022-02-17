921c85d34c45e038b1a3c61e714c1677d26c4a60
Aymane Silini <silinia@iut1i-702-a04.u-ga.fr>
2022-02-17 08:50:22 +0100

    recherche lignes et grid

--- ProjetTag/src/app/service/arret-filtre.pipe.ts
+++ ProjetTag/src/app/service/arret-filtre.pipe.ts
@@ -7,11 +7,11 @@ import { Arret } from './arret';
 export class ArretFiltrePipe implements PipeTransform {
 
   transform(items: Arret[], filter: string): any {
-
    if (!items || !filter){
      return items;
    } 
-   return items.filter(item => item.stopName.indexOf(filter) !== -1);
+   return items.filter(item => item.stopName.toUpperCase().indexOf(filter.toUpperCase()) !== -1 );
+
   }
 
 }
